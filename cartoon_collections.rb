b58e2a572615ef48540d0dc6cc7d8819fd565465
Chaya Deaver <chayadeaver@gmail.com>
2019-08-08 17:28:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,7 @@ def find_the_cheese(array)
   i = 0 
   while i < array.length do
     if array.include?(cheese_types[i])
-      return array[0]
+      return array[cheese_types[i]]
     else
       i+=1  
     end
